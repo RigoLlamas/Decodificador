b0c5fcf1f6f1658f436373e65a4f0f904431f28e
RigoLlamas <rigo.llamas77@gmail.com>
2026-03-04 18:32:43 -0600

    feat: add hexadecimal input via terminal and external file support
    - Implement direct hexadecimal instruction input from terminal
    - Add file reading support using "codigos.txt"
    - Enable automated processing of multiple instructions

--- main.c
+++ main.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "ISA.h"
 
-
-
-int main(){
+int main()
+{
+    char option = ' ';
     uint32_t codigo = 0;
-    printf("Ingresa instrucción en hexadecimal (ej: 002081B3): ");
-    scanf("%x",&codigo);
-    decodificar(codigo);
+    printf("Ingrese una opcion:\n[M] Manual \n[A] Archivo \n:");
+    scanf("%c", &option);
+    switch (option)
+    {
+    case 'M':
+    {
+        printf("Ingresa instrucciOn en hexadecimal (Ejemplo: 002081B3): ");
+        scanf("%x", &codigo);
+        printf("\n");
+        decodificar(codigo);
+    }
+    break;
+
+    case 'A':
+    {
+        FILE *archivo = fopen("codigos.txt", "r");
+        char instruccion[40] = " "; 
+        if (archivo == NULL)
+        {
+            printf("Error al abrir el archivo\n");
+            return 1;
+        }
+
+        while (fgets(instruccion, sizeof(instruccion), archivo))
+        {   
+            codigo = (uint32_t)strtoul(instruccion,NULL, 2);
+            decodificar(codigo);
+        }
+        fclose(archivo);
+    }
+    break;
+
+    default:
+        printf("Opcion invalida");
+        break;
+    }
+
     return 0;
 }
